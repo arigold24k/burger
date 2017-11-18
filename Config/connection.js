e6e55c58a02f3b7e49583edd2dce5d01b2e2a3a3
Ari Vidal <ariel.m.vidal@gmail.com>
2017-11-18 00:04:36 -0500

    adding jaws db

--- Config/connection.js
+++ Config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    //local host
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -17,6 +24,10 @@ connection.connect(function(err) {
     }
     console.log("connected as id " + connection.threadId);
 });
+
+
+
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
