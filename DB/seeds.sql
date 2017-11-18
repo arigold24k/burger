96c5a271b5d13591f7bbf9d2618fcbdc3f565bca
Ari Vidal <ariel.m.vidal@gmail.com>
2017-11-17 23:43:54 -0500

    commiting files

---
+++ DB/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swisss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Old Fashioned Burger', true);
